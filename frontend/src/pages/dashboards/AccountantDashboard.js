2a2fbac860d3b3e20a0390b5425df8b835b633bc
nizamshaik <nizamsk9631@gmail.com>
2026-08-03 10:36:47 +0530

    fix: Add settings route to AccountantDashboard

--- frontend/src/pages/dashboards/AccountantDashboard.js
+++ frontend/src/pages/dashboards/AccountantDashboard.js
@@ -16,6 +16,7 @@ import ConcessionManagement from '../components/ConcessionManagement';
 import PlanUpgradeRequired from '../components/PlanUpgradeRequired';
 import SchoolCalendarManagement from '../components/SchoolCalendarManagement';
 import InteractiveGoogleCalendar from '../../components/common/InteractiveGoogleCalendar';
+import SettingsManagement from '../components/SettingsManagement';
 
 // Shows a locked feature banner WITHIN a page (not a full block)
 const FeatureLockBanner = ({ featureName, requiredPlan = 'Gold' }) => (
@@ -429,6 +430,7 @@ const AccountantDashboard = ({ user, onLogout }) => {
           <Route path="concessions" element={<ConcessionManagement />} />
           <Route path="reports" element={<AccountantReports isPremiumFeatureAllowed={isPremiumFeatureAllowed} />} />
           <Route path="expenses" element={<AccountantExpenses />} />
+          <Route path="settings" element={<SettingsManagement />} />
           <Route path="*" element={<DashboardHome stats={stats} user={user} />} />
         </Routes>
       </div>
